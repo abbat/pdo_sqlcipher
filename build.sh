3960a233c4fd086e0bcf26e1ed0671da92407a9d
sergeypayu <sergeypayu@users.noreply.github.com>
2017-03-01 13:05:26 +0200

    Updated sed command to change VFS

--- build.sh
+++ build.sh
@@ -129,7 +129,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # change VFS type in
-sed -ie 's/i = sqlite3_open(filename, &H->db);/sqlite3_vfs_register(sqlite3_vfs_find("unix-excl"), 1); i = sqlite3_open(filename, &H->db);/g' "${BUILD_DIR}/sqlite_driver.c"
+sed -i 's/i = sqlite3_open(filename, &H->db);/sqlite3_vfs_register(sqlite3_vfs_find("unix-excl"), 1);\n&/' "${BUILD_DIR}/sqlite_driver.c"
 
 # magic :)
 for FILE in "${BUILD_DIR}"/*
